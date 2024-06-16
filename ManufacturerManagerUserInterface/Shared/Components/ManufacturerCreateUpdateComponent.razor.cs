ec4e299a1fd9509a79b35d91d70be93207f977d2
Julian Aburrow <jaburrow@btinternet.com>
2024-06-16 15:17:06 +0100

    Added component for Create/Update manufacturer

---
+++ ManufacturerManagerUserInterface/Shared/Components/ManufacturerCreateUpdateComponent.razor.cs
@@ -0,0 +1,8 @@
+﻿namespace ManufacturerManagerUserInterface.Shared.Components;
+
+public partial class ManufacturerCreateUpdateComponent
+{
+    [Parameter] public ManufacturerDisplayModel ManufacturerDisplayModel { get; set; } = null!;
+
+    [Parameter] public List<ManufacturerStatusModel> ManufacturerStatuses { get; set; } = null!;
+}
